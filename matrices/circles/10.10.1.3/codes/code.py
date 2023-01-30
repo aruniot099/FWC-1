35396218a3cfcfe05e101317bfe9264d93defb96
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-01-30 12:13:52 +0530

    Add files via upload

--- matrices/circles/10.10.1.3/codes/code.py
+++ matrices/circles/10.10.1.3/codes/code.py
@@ -1,1 +1,73 @@
+import numpy as np
+import mpmath as mp
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+
+import sys
+sys.path.insert(0,'/sdcard/FWC/CoordGeo')
+#path to my scripts
+
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+#from conics.funcs import circ_gen
+from conics.funcs import *
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+#Circle parameters
+r = 5
+OP=5
+OQ=12
+PQ=np.sqrt((OQ**2)-(OP**2))
+print(PQ)
+O = np.zeros(2)
+P =  np.array(([0,-r])) #normal vector
+Q =  np.array(([PQ,-5]))
+##Generating the line 
+xPQ = line_gen(P,Q)
+xOP = line_gen(O,P)
+xOQ = line_gen(O,Q)
+
+##Generating the circle
+x_circ= circ_gen(O,r)
+
+#Plotting all lines
+plt.plot(xPQ[0,:],xPQ[1,:],label='Tangent PQ')
+plt.plot(xOP[0,:],xOP[1,:],label='OP')
+plt.plot(xOQ[0,:],xOQ[1,:],label='OQ')
+
+#Plotting the circle
+plt.plot(x_circ[0,:],x_circ[1,:],label='Circle')
+
+
+#Labeling the coordinates
+tri_coords = np.vstack((O,P,Q)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['O','P','Q']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+#if using termux
+plt.savefig('../figs/fig.pdf')
+#plt.show()
+
+
+
+
+
+
 
