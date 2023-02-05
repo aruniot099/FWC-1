926023b62c9ab5bac79f69143c41a271798694c7
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-02-05 20:17:20 +0530

    Add files via upload

--- matrices/circles/10.10.1.3/codes/code3.py
+++ matrices/circles/10.10.1.3/codes/code3.py
@@ -37,36 +37,39 @@ print(a,b,c)
 x = np.roots([a, b, c]) 
 print(x) 
 #Q =  np.array(([5,np.sqrt(119)])) 
-Q1=A+x[0]*m 
-Q2=A+x[1]*m 
+Q2=A+x[0]*m 
+Q1=A+x[1]*m 
 print(Q1,Q2)
 
 ##Generating the line 
-xPQ = line_gen(P,Q2)
+xPQ1 = line_gen(P,Q1)
+xPQ2 = line_gen(P,Q2)
 xOP = line_gen(O,P)
-xOQ = line_gen(O,Q2)
+xOQ1 = line_gen(O,Q1)
+xOQ2 = line_gen(O,Q2)
 
 ##Generating the circle
 x_circ= circ_gen(O,r)
 
 #Plotting all lines
-plt.plot(xPQ[0,:],xPQ[1,:],label='Tangent PQ')
+plt.plot(xPQ1[0,:],xPQ1[1,:],label='Tangent PQ1')
+plt.plot(xPQ2[0,:],xPQ2[1,:],label='Tangent PQ2')
 plt.plot(xOP[0,:],xOP[1,:],label='OP')
-plt.plot(xOQ[0,:],xOQ[1,:],label='OQ')
-
+plt.plot(xOQ1[0,:],xOQ1[1,:],label='OQ1')
+plt.plot(xOQ2[0,:],xOQ2[1,:],label='OQ2')
 #Plotting the circle
 plt.plot(x_circ[0,:],x_circ[1,:],label='Circle')
 
 
 #Labeling the coordinates
-tri_coords = np.vstack((O,P,Q2)).T
+tri_coords = np.vstack((O,P,Q1,Q2)).T
 plt.scatter(tri_coords[0,:], tri_coords[1,:])
-vert_labels = ['O','P','Q']
+vert_labels = ['O','P','Q1','Q2']
 for i, txt in enumerate(vert_labels):
     plt.annotate(txt, # this is the text
                  (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
                  textcoords="offset points", # how to position the text
-                 xytext=(0,10), # distance from text to points (x,y)
+                 xytext=(10,-2), # distance from text to points (x,y)
                  ha='center') # horizontal alignment can be left, right or center
 
 plt.xlabel('$x$')
