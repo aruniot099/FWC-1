34c8c66148c7fcd6c696eb30377ebd2b2d932d77
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-02-14 00:12:12 +0530

    Add files via upload

--- vector-algebra/10.7.1.7/code/cprog/ques1.py
+++ vector-algebra/10.7.1.7/code/cprog/ques1.py
@@ -1,1 +1,56 @@
-
+import numpy as np 
+import matplotlib.pyplot as plt 
+from numpy import linalg as LA 
+import math 
+import sys     #for path to external scripts 
+  
+A = np.loadtxt('a.dat',dtype='float')
+B = np.loadtxt('b.dat',dtype='float')
+O = np.loadtxt('result.dat',dtype='float') 
+ 
+def line_gen(A,B): 
+   len =10 
+   dim = A.shape[0] 
+   x_AB = np.zeros((dim,len)) 
+   lam_1 = np.linspace(0,1,len) 
+   for i in range(len): 
+     temp1 = A + lam_1[i]*(B-A) 
+     x_AB[:,i]= temp1.T 
+   return x_AB 
+ 
+   
+x_OA = line_gen(O,A) 
+x_OB = line_gen(O,B) 
+ 
+ 
+ 
+#Plotting all lines 
+plt.plot(x_OA[0,:],x_OA[1,:],label='$OA$') 
+plt.plot(x_OB[0,:],x_OB[1,:],label='$OB$') 
+ 
+ 
+ 
+#Labeling the coordinates 
+tri_coords = np.vstack((A,B,O)).T 
+plt.scatter(tri_coords[0,:], tri_coords[1,:]) 
+vert_labels = ['A','B','O'] 
+for i, txt in enumerate(vert_labels): 
+    plt.annotate(txt, # this is the text 
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label 
+                 textcoords="offset points", # how to position the text 
+                 xytext=(0,10), # distance from text to points (x,y) 
+                 ha='center') # horizontal alignment can be left, right or center 
+ 
+plt.xlabel('$x-axis$') 
+plt.ylabel('$y-axis$') 
+plt.legend(loc='best') 
+plt.grid() # minor 
+plt.axis('equal') 
+plt.title('equidistant point',size=12) 
+plt.text(-2,9,'   (-2,9)') 
+plt.text(-7,0,'   (-7,0)') 
+plt.text(2,-5,'   (2,-5)') 
+#if using termux
+plt.savefig('/sdcard/FWC/figures/vectors/fig1.pdf')
+#else
+#plt.show()
---
+++ vector-algebra/10.7.1.7/code/cprog/sam.c
@@ -0,0 +1,31 @@
+/*Code by GVV Sharma (works on termux)
+Feb 13, 2023
+To find the point on x-axis which is equidistant from (2,-5) and (-2,9)*/
+#include<stdio.h>       
+#include<stdlib.h>
+#include<math.h>
+#include"lib.h"			//Functions
+int main()                 
+{
+	double **a,**b,**f,**g,**h,**x,**y,**p,**z;		//initializing the variables as matrices
+	double p1,c,c1,d,d1,e;
+	int m=2,n=1,i,j,s;
+	a=loadtxt("a.dat",2,1);		//loading the point A from the text file
+	b=loadtxt("b.dat",2,1);		//loading the point B from the text file
+	c=linalg_norm(a,m);		//Finding the norm of the matrix
+	d=linalg_norm(b,m);					
+	c1=c*c;				//finding the Square of the norm
+	d1=d*d;
+	e=c1-d1;			//Subtracting the norm values of both the input matrices
+	f=linalg_sub(a,b,m,n);		//Subtraction of the matrices
+	g=transpose(f,m,n);		//Finding the Transpose of the matrix
+	x=loadtxt("2.dat",m,n);		//loading the 2  from the .dat file
+	h=matmul(x,g,m,n,2);
+	y=loadtxt("e1.dat",2,1);	//loading the e1 (1,0) from the .dat file
+	p=matmul(y,h,m,n,2);
+	p1=p[0][0];
+	s=e/p1;				//dividing the numerator with denominator
+	z=mult_int(s,y,m,n);
+	print(z,m,n);			//printing the result
+	save(z,m,n);			//saving the result to the figure
+}
