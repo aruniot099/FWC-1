571ec1ac9c457bba1e79fc4cfcfe7d02b7fd23e3
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-02-16 21:06:30 +0530

    changes

--- vector-algebra/10.7.2.4/codes/cprog/main1.c
+++ vector-algebra/10.7.2.4/codes/cprog/main1.c
@@ -14,9 +14,9 @@ int main() {
     b = loadtxt("b.dat", 2, 1);   // Loading the point B from the text file
     c = loadtxt("c.dat", 2, 1);   // Loading the point C from the text file
     
-    x1 = linalg_sub(a,c,m,n);
-    x2 = linalg_sub(c,b,m,n);
-    x  = linalg_div(x1,x2,m,n);
+    x1 = linalg_sub(a,c,m,n);     // Subtracting the matrices A and C and storing the result in x1
+    x2 = linalg_sub(c,b,m,n);     // Subtracting the matrices C and B and storing the result in x2
+    x  = linalg_div(x1,x2,m,n);   // Dividing the matrices x1 and x2 and storing the result in x 
 
     printf("Point (-1,6) divides PQ in ratio of %lf",x[0][0]);
     // Return the value of x to be used in Python
