3c9b15f0f7a59b90baba4030a86a4ac80ce2aa57
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-02-17 12:17:08 +0530

    Add files via upload

---
+++ vector-algebra/10.7.4.6/codes/cprog/lib.h
@@ -0,0 +1,290 @@
+//Function declaration
+double **createMat(int m,int n);
+void readMat(double **p, int m,int n);
+void print(double **p,int m,int n);
+double **loadtxt(char *str,int m,int n);
+double linalg_norm(double k);
+double **linalg_sub(double **a, double **b, int m, int n);
+double **linalg_inv(double **mat, int m);
+double **matmul(double **a, double **b, int m, int n, int p);
+double **transpose(double **a,  int m, int n);
+double cross_prod(double **a,double **b);
+//End function declaration
+
+
+//Defining the function for matrix creation
+double **createMat(int m,int n)
+{
+ int i;
+ double **a;
+ 
+ //Allocate memory to the pointer
+a = (double **)malloc(m * sizeof( *a));
+    for (i=0; i<m; i++)
+         a[i] = (double *)malloc(n * sizeof( *a[i]));
+
+ return a;
+}
+//End function for matrix creation
+
+
+//Defining the function for reading matrix 
+void readMat(double **p, int m,int n)
+{
+ int i,j;
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   scanf("%lf",&p[i][j]);
+  }
+ }
+}
+//End function for reading matrix
+
+//Read  matrix from file
+double **loadtxt(char *str,int m,int n)
+{
+FILE *fp;
+double **a;
+int i,j;
+
+
+a = createMat(m,n);
+fp = fopen(str, "r");
+
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   fscanf(fp,"%lf",&a[i][j]);
+  }
+ }
+//End function for reading matrix from file
+
+fclose(fp);
+ return a;
+
+}
+
+
+//Defining the function for printing
+void print(double **p, int m,int n)
+{
+ int i,j;
+
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  printf("%lf ",p[i][j]);
+ printf("\n");
+ }
+}
+//End function for printing
+
+//Defining the function for norm
+
+double linalg_norm(double k)
+{
+int i;
+double norm;
+norm=k*k;
+return sqrt(norm);
+
+}
+
+//End function for norm
+
+//Defining the function for difference of matrices
+
+double **linalg_sub(double **a, double **b, int m, int n)
+{
+int i, j;
+double **c;
+c = createMat(m,n);
+
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+c[i][j]= a[i][j]-b[i][j];
+  }
+ }
+return c;
+
+}
+//End function for difference of matrices
+
+//Defining the function for inverse of 2x2 matrix
+
+
+double **linalg_inv(double **mat, int m)
+{
+double **c, det;
+c = createMat(m,m);
+
+det = mat[0][0]*mat[1][1]-mat[0][1]*mat[1][0];
+
+c[0][0] = mat[1][1]/det;
+c[0][1] = -mat[1][0]/det;
+c[1][0] = -mat[0][1]/det;
+c[1][1] = mat[0][0]/det;
+
+return c;
+
+}
+// End  function for inverse of 2x2 matrix
+
+
+//Defining the function for difference of matrices
+
+double **matmul(double **a, double **b, int m, int n, int p)
+{
+int i, j, k;
+double **c, temp =0;
+c = createMat(m,p);
+
+ for(i=0;i<m;i++)
+ {
+  for(k=0;k<p;k++)
+  {
+    for(j=0;j<n;j++)
+    {
+	temp= temp+a[i][j]*b[j][k];
+    }
+	c[i][k]=temp;
+	temp = 0;
+  }
+ }
+return c;
+
+}
+//End function for difference of matrices
+
+//Defining the function for transpose of matrix
+
+double **transpose(double **a,  int m, int n)
+{
+int i, j;
+double **c;
+//printf("I am here");
+c = createMat(n,m);
+
+ for(i=0;i<n;i++)
+ {
+  for(j=0;j<m;j++)
+  {
+c[i][j]= a[j][i];
+//  printf("%lf ",c[i][j]);
+  }
+ }
+return c;
+
+}
+//End function for transpose of matrix
+double mat_mult(double **a, double **b, int m,int n, int p)
+{
+int i, j, k;
+double c, temp =0;
+//c = createMat(m,p);                                                            
+for(i=0;i<m;i++)
+ {
+  for(k=0;k<p;k++)
+  {
+    for(j=0;j<n;j++)
+    {
+        temp= temp+a[i][j]*b[j][k];
+    }
+        c=temp;
+        temp = 0;
+  }
+ }
+return c;
+
+}
+
+double **mult_int(int scalar,double **a,int m, int n)
+{
+  int i, j;
+  for(i=0;i<m;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      a[i][j] *= scalar;
+    }
+  }
+  return a;
+}
+
+void save(double **a, int rows, int cols) {
+  FILE *fp;
+  fp = fopen("result.dat", "w");
+  //fprintf(fp, "The result of the multiplication is:\n");
+  int i, j;
+  for (i = 0; i < rows; i++) {
+    //fprintf(fp, "[");
+    for (j = 0; j < cols; j++) {
+      fprintf(fp, "%lf", a[i][j]);
+      if (j < cols - 1) {
+        //fprintf(fp, ", ");
+      }
+    }
+    fprintf(fp, "\n");
+  }
+  fclose(fp);
+}
+
+void save1(double **a, int rows, int cols) {
+  FILE *fp;
+  fp = fopen("result1.dat", "w");
+  //fprintf(fp, "The result of the multiplication is:\n");
+  int i, j;
+  for (i = 0; i < rows; i++) {
+    //fprintf(fp, "[");
+    for (j = 0; j < cols; j++) {
+      fprintf(fp, "%lf", a[i][j]);
+      if (j < cols - 1) {
+        //fprintf(fp, ", ");
+      }
+    }
+    fprintf(fp, "\n");
+  }
+  fclose(fp);
+}
+
+double cross(double **a, double **b)
+{
+
+ return (a[0][0])*(b[1][0])-(b[0][0])*(a[1][0]);
+}
+
+double **mult_float(float scalar,double **a,int m, int n)
+{
+  int i, j;
+  for(i=0;i<m;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      a[i][j] *= scalar;
+    }
+  }
+  return a;
+}
+
+double **linalg_add(double **a, double **b,
+int m, int n)
+{
+int i, j;
+double **c;
+c = createMat(m,n);
+
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+c[i][j]= a[i][j]+b[i][j];
+  }
+ }
+return c;
+
+}
--- vector-algebra/10.7.4.6/codes/cprog/main.c
+++ vector-algebra/10.7.4.6/codes/cprog/main.c
@@ -1,1 +1,44 @@
+/*Code by G. Arun Kumar (works on termux)
+Feb 17, 2023
+Link to License("https://github.com/aruniot099/FWC-1/blob/main/License")
+Calculating the area of the ADE where the vertices of triangle ABC are A (4, 6), B (1, 5) and C (7, 2). A line is drawn to intersectsides AB and AC at D and E respectively ,such that AD/AB= AE/AC = 1/4.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "lib.h" // User-defined functions
+
+int main()
+{
+    // Initialize the variables as matrices
+    double **a, **b, **c, **d, **a1, **e, **f;
+    double area1, area2;
+    float x, x1, z;
+    int m = 2, n = 1;
+
+    // Load points from text files
+    a = loadtxt("a.dat", 2, 1);
+    b = loadtxt("b.dat", 2, 1);
+    c = loadtxt("c.dat", 2, 1);
+
+    // Calculate point D and E using section formula
+    x = 0.3333333333333; // Ratio is given as 3/2
+    d = mult_float(1 / (1 + x), linalg_add(a, mult_float(x, b, m, n), m, n), m, n);
+    e = mult_float(1 / (1 + x), linalg_add(a, mult_float(x, c, m, n), m, n), m, n);
+
+    // Saving the results
+    save(d,m,n);
+    save1(e,m,n);
+
+    // Calculate area of triangles ADE and ABC
+    area1 = 0.5 * linalg_norm(cross(linalg_sub(a, d, m, n), linalg_sub(a, e, m, n)));
+    area2 = 0.5 * linalg_norm(cross(linalg_sub(a, b, m, n), linalg_sub(a, c, m, n)));
+
+    // Print the results
+    printf("Area of triangle ADE: %lf\n", area1);
+    printf("Area of triangle ABC: %lf\n", area2);
+
+    return area1;
+}
 
